0f8bfcde30a61d0f08273436f77887525f4625d4
Peter Agholor <peter.agholor@sterling.ng>
2024-08-28 17:12:36 +0100

    Meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,14 +13,14 @@ const lato = Lato({
 });
 
 export const metadata: Metadata = {
-  title: "Peter A.O",
-  description: "Peter's Portfolio",
+  title: "Webbro-Consults",
+  description: "Website Bro Consults",
   openGraph: {
-    title: "Peter A.O",
-    description: "Peter's portfolio",
+    title: "Webbro-Consults",
+    description: "Website Bro Consults",
     images: [
       {
-        url: "https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/376266403_187352477705052_2218369533813198303_n.jpg?stp=dst-jpg_s1080x2048&_nc_cat=107&ccb=1-7&_nc_sid=49d041&_nc_ohc=7k_OpXospQ0AX-d_aJZ&_nc_ht=scontent-los2-1.xx&oh=00_AfCtm6vG1pGzvvs6WSFjLuq1hrfyI_-fTnuyRHzb8A1NhQ&oe=6501A176",
+        url: "",
       },
     ],
   },
